42eec6aef442bd28e971142be452cd68cb1bb835
DrinkCupTea <tianjiangjianpo@126.com>
2022-03-17 23:57:58 +0800

    Init, Basic of 2048

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import GamePageVue from './components/GamePage.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <GamePageVue></GamePageVue>
 </template>
 
 <style>
+body {
+  margin: 0;
+  height: 100%;
+  background-color: rgb(52, 108, 156);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/GamePage.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const name: String = "xie"
+let array: Array<Number> = []
+for (let i = 0; i < 16; i++) {
+  array.push(i)
+}
+</script>
+
+<template>
+<div>
+  <p id="title">2048</p>
+  <div id="container">
+    <div v-for="i in array"></div>
+  </div>
+</div>
+</template>
+
+<style scoped >
+#container {
+  /* Background Color */
+  background-color: rgba(230, 230, 230, 0.75);
+  /* Put it at center */
+  margin-left: auto;
+  margin-right: auto;
+  /* Padding */
+  padding-top: 1vw;
+  padding-left: 1vw;
+  /* Size of container */
+  width: 32vw;
+  height: 32vw;
+  /* Gird */
+  display: grid;
+  grid: repeat(4, 8vw) / auto-flow 8vw;
+  /* justify-items: center; */
+  /* Radius */
+  border-radius: 1vw;
+}
+
+#title {
+  color: white;
+  font-size: large;
+}
+
+#container > div {
+  background-color: rgb(147, 187, 207);
+  border-radius: 1vw;
+  width: 7vw;
+  height: 7vw;
+}
+</style>
